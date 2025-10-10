cde40309826553843b84553b658a0b043325826a
Alucardvolxe <gogamerdan247@gmail.com>
2025-10-10 10:28:08 +0000

    finishing the bookmanagement assignment

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .models import Book
+from .forms import BookForm
+#Displaying the books
+def book_list(request):
+    books= Book.objects.all()
+    return render(request,"list.html",{"books":books})
+    
+
+#Creating a new book
+def book_create(request):
+    if request.method =="POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+    return render(request, "create.html", {"form": form})
+
+#Update a book
+def book_update(request,pk):
+    book= get_object_or_404(Book,pk=pk)
+    if request.method=="Post":
+        form=BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm(instance=book)
+    return render(request, "update.html",{"form",form})
+
+
+#Delete a book
+
+def book_delete(request,pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method =="POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request,"delete.html",{"book":book})
